8dd74fd92c1d00eb47feb40a707a3c924cb0c52b
Emrah ALTINDİŞ <emrah_altindis@hotmail.com>
2021-04-04 14:41:59 +0300

    M003 Error on Linear and Polynomial Regression

---
+++ M003PolynomialRegressionError.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv("M001\\linear.csv")  # Verimizi okuyalım
+
+x = data["metrekare"]
+y = data["fiyat"]
+
+x = np.array(x)
+y = np.array(y)
+
+a,b,c,d = np.polyfit(x,y,3)
+
+
+z = np.arange(150)
+
+plt.scatter(x,y)
+
+plt.plot(z,a*z*z*z+b*z*z+c*z+d)
+
+
+z = int(input("metrekare="))
+sonuc = a*z**3+b*z**2+c*z+d
+print(sonuc)
+
+plt.scatter(z,sonuc,c="red",marker=">")
+plt.show()
+
+for i in range(int(len(y))):
+    errorPolynomialSqryt =+ (y[i] - (a*(x[i])**3+b*(x[i])**2+c*x[i]+d))**2
+
+print("Polynomial Error = " , errorPolynomialSqryt)
